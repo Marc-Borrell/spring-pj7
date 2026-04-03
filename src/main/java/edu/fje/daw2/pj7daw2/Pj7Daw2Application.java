f23bbd017523ccc8a3df23f7da676e2a005a7687
marcboso2005 <marcboso2005@gmail.com>
2026-04-03 12:00:53 +0200

    creació del projecte amb totes les dependencies necesaries amb Java25

---
+++ src/main/java/edu/fje/daw2/pj7daw2/Pj7Daw2Application.java
@@ -0,0 +1,13 @@
+package edu.fje.daw2.pj7daw2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Pj7Daw2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Pj7Daw2Application.class, args);
+	}
+
+}
